6484202d0454400adc7d6816eb594e5a4c8a108e
jananonuevo <jananonuevo7@gmail.com>
2025-08-16 12:13:27 +0800

    fix responsiveness 1213

--- src/components/landing/Aboutme.tsx
+++ src/components/landing/Aboutme.tsx
@@ -2,9 +2,9 @@ function Aboutme() {
     
     return (
         <>
-        <div className='mt-30 lg:mt-10 mx-30 flex flex-col justify-center items-center text-center'>
+        <div className='mt-30 lg:mt-10 flex flex-col justify-center items-center text-center'>
             <h1 className="text-5xl font-bold text-[#292929]">About Me</h1>
-            <div className="text-md lg:text-2xl mx-2 lg:mx-10 lg:mt-10 mt-5 text-[#292929] leading-10">I'm Jan Nielsen Añonuevo, a Computer Science graduate from Mapua University with five years of programming experience. My passion for software development began in Junior High at Don Bosco Technical Institute of Makati, where I specialized in Computer Technology. I am proficient in C/C++, PHP, Java, HTML, CSS, JavaScript, SQL, and Python, and familiar with frameworks like Bootstrap, Tailwind CSS, and ReactJS. My strong foundation in programming allows me to quickly adapt to new languages and technologies.</div>
+            <div className="text-md lg:text-2xl mx-5 lg:mx-15 lg:mt-10 mt-5 text-[#292929] leading-8 lg:leading-10">I'm Jan Nielsen Añonuevo, a Computer Science graduate from Mapua University with five years of programming experience. My passion for software development began in Junior High at Don Bosco Technical Institute of Makati, where I specialized in Computer Technology. I am proficient in C/C++, PHP, Java, HTML, CSS, JavaScript, SQL, and Python, and familiar with frameworks like Bootstrap, Tailwind CSS, and ReactJS. My strong foundation in programming allows me to quickly adapt to new languages and technologies.</div>
         </div>
         </>
     )
