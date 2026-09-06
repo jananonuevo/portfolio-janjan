33dd8640cf194217207a6ce15d33df24ccd1840c
juanbagongtaon <jananonuevo7@gmail.com>
2026-09-06 13:12:06 +0800

    pointed repo to project

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()
-  ],
+  base: '/portfolio-janjan/', // Required for GitHub Pages project sites
+  plugins: [react(), tailwindcss()],
 })
